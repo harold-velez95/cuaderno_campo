02233f5263a38034a0a9b06382a00cf68c527dbf
Harold Mauricio Velez Zapata <haroldvelez@MacBook-Air-de-Harold.local>
2025-12-10 22:52:14 +0100

    import os

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for
 from models import Crear, Equipos, Infraestructura, Personal, Inventario, Ugestion, Costos, Tareas
 import db
+import os
 
 app = Flask(__name__)
 
